8123b3d961979739396608b9a6654c9742e7dd55
RusterYBT <3011647435@qq.com>
2026-09-16 21:36:43 +0800

    建立架构

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+trait AgentTrait {
+    fn get_name(&self) -> &str;
+    fn get_version(&self) -> &str;
+}
+struct Agent {
+    name: String,
+    version: String,
+}
+impl AgentTrait for Agent {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    fn get_version(&self) -> &str {
+        &self.version
+    }
+}
+fn get_agent(agent: &impl AgentTrait){
+    print!("Agent: {}, Version: {}", agent.get_name(), agent.get_version());
+}
 fn main() {
-    println!("Hello, world!");
+    let agent = Agent {
+        name: "MyAgent".into(),
+        version: "1.0.0".into(),
+    };
+    get_agent(&agent);
 }
